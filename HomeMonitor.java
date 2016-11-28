cdda3a80aa88819cce9f4fac5a18ace3558b9360
Ivy Chang <ivycwq@icloud.com>
2016-11-27 20:27:58 -0800

    adding remote implementation

---
+++ HomeMonitor.java
@@ -0,0 +1,9 @@
+import java.rmi.*;
+import java.rmi.server.*;
+ 
+public class HomeMonitor
+    extends UnicastRemoteObject implements GumballMachineRemote {
+
+    private static final long serialVersionUID = 2L;
+
+}
