5026cf1b0e1c5f23bce46e702fcc22210bf12d03
Ivy Chang <ivycwq@icloud.com>
2016-11-27 20:36:01 -0800

    adding sub systems

---
+++ HomeLightsSystem.java
@@ -0,0 +1,8 @@
+public class HomeLightsSystem extends System {
+  public void turnOff() {
+    System.out.println("Home Lights System is Off.");
+  }
+  public void turnOn() {
+    System.out.println("Home Lights System is On.");
+  }
+}
---
+++ HomeTemperatureSystem.java
@@ -0,0 +1,8 @@
+public class HomeTemperatureSystem extends System {
+  public void turnOff() {
+    System.out.println("Home Temperature Control System is Off.");
+  }
+  public void turnOn() {
+    System.out.println("Home Temperature Control System is On.");
+  }
+}
